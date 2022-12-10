f346ec5f763d9ff51e47b2f0d6f7b99b377e9c86
Manuel Pautasso <manuelpautasso@gmail.com>
2022-12-10 16:18:04 -0300

    First commit

---
+++ src/main/java/com/mpautasso/homebanking/HomebankingCaCSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mpautasso.homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingCaCSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingCaCSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpautasso/homebanking/HomebankingCaCSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpautasso.homebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankingCaCSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
