8b6d64fd332258c0203d3361c15ac4575dc13478
Manuel Pautasso <manuelpautasso@gmail.com>
2022-12-30 18:49:26 -0300

    added account and transaction repositories

--- src/main/java/com/mpautasso/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mpautasso/homebanking/repositories/AccountRepository.java
@@ -1,9 +1,16 @@
 package com.mpautasso.homebanking.repositories;
 
 import com.mpautasso.homebanking.models.Account;
+import com.mpautasso.homebanking.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    List<Account> findAllByUser(User user);
+    Optional<Account> findByNumber(Integer number);
+    Optional<Account> findByCbu(Integer cbu);
 }
--- src/main/java/com/mpautasso/homebanking/repositories/TransactionRepository.java
+++ src/main/java/com/mpautasso/homebanking/repositories/TransactionRepository.java
@@ -1,9 +1,16 @@
 package com.mpautasso.homebanking.repositories;
 
+import com.mpautasso.homebanking.models.Account;
 import com.mpautasso.homebanking.models.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+
+    List<Transaction> findByAccountReceiver(Account account);
+
+    List<Transaction> findByAccountSender(Account account);
 }
